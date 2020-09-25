57d861fe51eb8c54627a7fcbc16f7875e6f63d73
Jorge <jorgediazok@gmail.com>
2020-09-25 13:38:50 -0300

    added links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      Hello People!
+      <div>
+        <Link to="/blog">blog page</Link>
+      </div>
+      <div>
+        <Link to="https://www.gatsbyjs.org">gatsby docs</Link>
+      </div>
+    </div>
+  )
 }
