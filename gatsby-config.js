b22c6aa11ba851919f17e5acfd3561587a519716
Jorge <jorgediazok@gmail.com>
2020-09-26 02:01:35 -0300

    targetting posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,5 +23,12 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
+      },
+    },
   ],
 }
