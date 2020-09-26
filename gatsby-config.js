4566480894e85805a7c557ed13cf45203cbc6038
Jorge <jorgediazok@gmail.com>
2020-09-26 00:20:31 -0300

    experimenting with graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,12 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "gatsby tutorial",
+    description: "some random description",
+    author: "@John_Doe",
+    data: ["item 1", "item 2", "item 3"],
+    person: { name: "peter", age: 32 },
+  },
   plugins: [`gatsby-plugin-styled-components`],
 }
