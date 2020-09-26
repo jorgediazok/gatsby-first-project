6d96dcf2562fa878e6c76538857f305477ad8fd6
Jorge <jorgediazok@gmail.com>
2020-09-25 21:12:10 -0300

    adding footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       Hello People!
       <div>
         <Link to="/blog">blog page</Link>
@@ -11,6 +14,7 @@ export default function Home() {
       <div>
         <Link to="https://www.gatsbyjs.org">gatsby docs</Link>
       </div>
+      <Footer />
     </div>
   )
 }
