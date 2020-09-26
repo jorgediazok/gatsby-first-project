cb51847a4c06d8e14575e218d7d48e7c26f6a5b6
Jorge <jorgediazok@gmail.com>
2020-09-25 23:29:02 -0300

    added styled-components

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,6 +5,12 @@ const blog = () => {
   return (
     <Layout>
       <h1>This is the blog page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus quae
+        ab et consequuntur quis velit, harum molestias temporibus illum,
+        distinctio sequi facilis. At alias dolorem id necessitatibus impedit
+        fuga expedita.
+      </p>
     </Layout>
   )
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -6,6 +6,12 @@ export default class products extends Component {
     return (
       <Layout>
         <h1>Hello from products</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime ut
+          dolor mollitia, labore, pariatur, impedit velit illo iure repellendus
+          quaerat quisquam corporis laudantium dolorum. Aspernatur, magni. Autem
+          unde quasi distinctio!
+        </p>
       </Layout>
     )
   }
